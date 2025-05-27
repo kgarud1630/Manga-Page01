9eed7a181b8111d1823aebe45d23bb4b62832dfc
kgarud1630 <kgarud1630@gmail.com>
2025-05-27 12:13:46 +0530

    Create User Settings Page

---
+++ src/pages/user/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserSettings: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">User Settings</h1>
+      <p>This is the user settings page where users can update preferences or profile info.</p>
+    </div>
+  );
+};
+
+export default UserSettings;
