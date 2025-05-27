9994a6a3698ccdfd8c64785884bb8fbf195f1721
kgarud1630 <kgarud1630@gmail.com>
2025-05-27 12:10:28 +0530

    Create Library.tsx

---
+++ src/pages/user/Library.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserLibrary: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">User Library</h1>
+      <p>This is the user's library page. Books, anime, or content can be listed here.</p>
+    </div>
+  );
+};
+
+export default UserLibrary;
